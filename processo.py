e2b811a33501d557a124e039a1db77247bcaca26
Arthur Diniz <adl.csrss.exe1999@gmail.com>
2019-06-05 13:09:10 -0300

    trabalho concluído

--- processo.py
+++ processo.py
@@ -1,32 +1,5 @@
 class Processo:
-    def __init__(self, nome: str, id: int, valor):
-        self.nome = str
-        self.id = int
-        self.valor = []
-
-    @property
-    def nome(self) -> str:
-        return self.nome
-
-    @nome.setter
-    def nome(self, nome: str):
+    def __init__(self, nome: str, pid: int, array):
         self.nome = nome
-
-    @property
-    def id(self) -> int:
-        return self.id
-
-    @id.setter
-    def id(self, value: int):
-        self.id = value
-
-    @property
-    def valor(self) -> list:
-        return self.valor
-
-    @valor.setter
-    def valor(self, valor_add: int):
-        if len(self.valor) <= 10:
-            self.valor.append(valor_add)
-        else:
-            raise ValueError("Array cheio")
+        self.pid = pid
+        self.array = array
--- regiao_critica.py
+++ regiao_critica.py
@@ -2,23 +2,14 @@ class RegiaoCritica:
     def __init__(self):
         self.vetor = []
 
-    @property
-    def vetor(self) -> list:
-        return self.vetor
-
-    @vetor.setter
-    def vetor(self, valor: list):
-        self.vetor_add(valor)
-
-    def vetor_add(self, lista: list):
-        if len(lista) + len(self.vetor) <= 1000:
-            for valor in lista:
-                self.vetor.append(valor)
+    def vetor_add(self, valor: int):
+        if len(self.vetor) < 1000:
+            self.vetor.append(valor)
         else:
-            raise OverflowError(F"Vetor cheio! {len(self.vetor)} elementos.")
+            raise IndexError("região critica cheia")
 
     def vetor_rem(self):
         if self.vetor:
             self.vetor.pop(0)
         else:
-            raise IndexError("Vetor vazio.")
+            raise IndexError("região critica vazia")
